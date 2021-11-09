ae5ccdb0169f147deae83a7c311d92599f70aed0
OlafScholten <49287122+OlafScholten@users.noreply.github.com>
2021-11-09 16:23:06 +0100

    Add files via upload

--- ShortCuts.sh
+++ ShortCuts.sh
@@ -1,11 +1,13 @@
 # Define some importants shortcuts
 ProgramDir="../Program/"
 UtilDir="../Utilities/"
-export AntennaFieldsDir="../AntennaFields/"
+# point to directory where HDF5 utilities are stored
+HDF5prefix="/usr/local/hdf5"   
 LIBRARY="-lm ~/NumLib/bin/libfftpack5.1d.a"
 export AntennaFun="../AntenFunct/v2-"
+export AntennaFieldsDir="../AntennaFields/"
 FlashFolder=$(pwd)
-Vrsn="-v21"  # version number
+Vrsn=""  # version number
 #  It is essential not to have spaces around the = sign in the previous commands
 echo "FlashFolder:" ${FlashFolder}
 echo "Antenna:" ${AntennaFieldsDir}
