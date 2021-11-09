cb630dc440a8116fdad0b437f1593730af3e9eb6
OlafScholten <49287122+OlafScholten@users.noreply.github.com>
2021-11-09 09:22:59 +0100

    Add files via upload

---
+++ ShortCuts.bat
@@ -0,0 +1,11 @@
+:: Define some importants shortcuts
+set ProgramDir=..\Program\
+set UtilDir=..\Utilities\
+::set LIBRARY="C:\OlafsUtil\NumLib\bin\libFFTPack-d.a"
+set "LIBRARY=-lm C:\OlafsUtil\NumLib\bin\libFFTPack-d.a"
+set "AntennaFun=..\AntenFunct\"
+set FlashFolder=%cd%
+set "Vrsn=-v21"  
+Echo "FlashFolder:" %FlashFolder%
+
+::  pause
---
+++ ShortCuts.sh
@@ -0,0 +1,11 @@
+# Define some importants shortcuts
+ProgramDir="../Program/"
+UtilDir="../Utilities/"
+export AntennaFieldsDir="../AntennaFields/"
+LIBRARY="-lm ~/NumLib/bin/libfftpack5.1d.a"
+export AntennaFun="../AntenFunct/v2-"
+FlashFolder=$(pwd)
+Vrsn="-v21"  # version number
+#  It is essential not to have spaces around the = sign in the previous commands
+echo "FlashFolder:" ${FlashFolder}
+echo "Antenna:" ${AntennaFieldsDir}
