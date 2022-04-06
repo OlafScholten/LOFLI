f66bf3f561af47c9a706a8519ceeaa9c2793f7b9
OlafScholten <49287122+OlafScholten@users.noreply.github.com>
2022-04-06 14:02:28 +0200

    Add files via upload

--- copysetup/PowerTrace.sh
+++ copysetup/PowerTrace.sh
@@ -5,7 +5,9 @@ source ../ShortCuts.sh
 Prog="PowerTrace"
 source ${UtilDir}/compile.sh
 
+# start Exploration run
 ${ProgramDir}/$Prog  <PwT.in
+#gle -d pdf -o Map_Explore.pdf ${UtilDir}/LeaderTrack.gle ${FlashFolder}/Explore
 exit
 # read -rsp $'Press enter to continue...\n'
 #sshfs scholten@kapteyn.astro.rug.nl:/net/dataserver3/data/users/hare/ ~/kaptdata
