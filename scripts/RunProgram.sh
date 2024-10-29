75fa1899429bd7608a6caddb2a0b92cef7c4fd31
OlafScholten <49287122+OlafScholten@users.noreply.github.com>
2024-10-29 14:49:03 +0100

    Add files via upload

--- scripts/RunProgram.sh
+++ scripts/RunProgram.sh
@@ -1,23 +1,27 @@
-#!/bin/bash
+#!/bin/bash 
 # 
 # 
 source  ${LL_Base}/ShortCuts.sh
 export Prog=$1
-export input=$2
 
-echo "program run is: ${Prog} with argument ${input}" 
+echo "program run is: ${Prog} with argument $2" 
 
 if ! test -f ${LL_bin}/${Prog}; then
    echo ${Prog} "does not exist and will be generated."
    source ${LL_scripts}/compileHDF5program.sh
 fi
 
-if test -f ${input}; then
-   echo "File ${input} exists"
-   echo "executing command: ${LL_bin}/${Prog}  <${input} "
-   ${Prog}  <${input}
+if test -f $2; then
+   echo "File $2 exists"
+   export input=' <'$2   
+   echo "executing command: ${LL_bin}/${Prog}  ${input} "
+   ${LL_bin}/${Prog}  <$2   # This is apparently very different from    ${LL_bin}/${Prog}  ${input}
 else
    echo "arguments ${input} is not an existing file"
+   export input=$2
    echo "executing command: ${LL_bin}/${Prog}  ${input} "
-   ${Prog}  ${input}
+   ${LL_bin}/${Prog}  $2
 fi
+
+#echo "executing command: ${LL_bin}/${Prog}  ${input} "
+#${LL_bin}/${Prog}  ${input}
