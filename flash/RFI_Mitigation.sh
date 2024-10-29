b54da4e605bd14c9c0e14b53cc57ecf6f359e5d3
OlafScholten <49287122+OlafScholten@users.noreply.github.com>
2024-10-29 14:50:35 +0100

    Add files via upload

--- flash/RFI_Mitigation.sh
+++ flash/RFI_Mitigation.sh
@@ -7,6 +7,7 @@ source  ${LL_Base}/scripts/RunProgram.sh RFI_Mitigation  2
 echo "Start exploration run"
 nohup ./Explore.sh  >Explore.log 2>&1  &   
 echo "Explore was submitted, now wait."
+echo ${ArchiveBase}
 
 exit
 #sshfs scholten@kapteyn.astro.rug.nl:/net/dataserver3/data/users/hare/ ~/kaptdata
